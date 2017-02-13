fa02603ac3e593df55bcd5e9a6698be86ecd401a
parweb <parweb@gmail.com>
2017-02-13 17:02:45 +0100

    ajout des champs necessaire sur la table orders

--- sql/install.php
+++ sql/install.php
@@ -2,10 +2,12 @@
 
 $sql = [];
 
-$sql[] = 'CREATE TABLE IF NOT EXISTS `' . _DB_PREFIX_ . 'quotes` (
-		`id_quote` int(10) unsigned NOT NULL AUTO_INCREMENT,
-		PRIMARY KEY  (`id_quote`)
-) ENGINE=' . _MYSQL_ENGINE_ . ' DEFAULT CHARSET=utf8;';
+$sql[] = 'ALTER TABLE `' . _DB_PREFIX_ . 'orders`
+ADD `utm_source` VARCHAR(255) NOT NULL AFTER `dpd`,
+ADD `utm_medium` VARCHAR(255) NOT NULL AFTER `utm_source`,
+ADD `utm_campaign` VARCHAR(255) NOT NULL AFTER `utm_medium`,
+ADD `utm_term` VARCHAR(255) NOT NULL AFTER `utm_campaign`,
+ADD `utm_content` VARCHAR(255) NOT NULL AFTER `utm_term`;';
 
 foreach ( $sql as $query ) {
 	if ( Db::getInstance()->execute( $query ) == false ) {
--- sql/uninstall.php
+++ sql/uninstall.php
@@ -2,7 +2,12 @@
 
 $sql = [];
 
-$sql[] = 'DROP TABLE IF EXISTS `' . _DB_PREFIX_ . 'quotes`;';
+$sql[] = 'ALTER TABLE `' . _DB_PREFIX_ . 'orders`
+DROP `utm_source`,
+DROP `utm_medium`,
+DROP `utm_campaign`,
+DROP `utm_term`,
+DROP `utm_content`;';
 
 foreach ( $sql as $query ) {
 	if ( Db::getInstance()->execute( $query ) == false ) {
